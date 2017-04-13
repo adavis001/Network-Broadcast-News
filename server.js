af0d631acdd3acd533035fcae9da0f2f2fa91c61
Alexander Davis <akonidavis@gmail.com>
2017-04-12 18:59:50 -1000

    usernames online

--- server.js
+++ server.js
@@ -6,11 +6,22 @@ let clientPool = [];
 const server = net.createServer((connection) => {
 	clientPool.push(connection);
 	console.log('new connection!');
-	
+	console.log(connection.id);
+	const stdin = process.stdin, stdout = process.stdout;
+
+
+	connection.write("Insert user name");
+
+	connection.once('data', function(data){
+		stringData = data.toString().trim();
+		connection.id = stringData;
+		console.log(connection.id);
+	});
+
 	connection.on('data', (data)=>{
 	console.log(data.toString());
 		for(let i = 0; i<clientPool.length; i++){
-			clientPool[i].write(data);		
+			clientPool[i].write(connection.id + ":" + data);		
 		}
 	});
 
